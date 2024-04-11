d51688248323bd6b9f0339dc2f035730b544ad65
sofia2ano <166713618+sofia2ano@users.noreply.github.com>
2024-04-11 12:02:33 -0300

    primeirocommit

---
+++ script/script.js
@@ -0,0 +1,73 @@
+const produtos = [
+    {
+        Titulo: "Tênis Adidas Response Runner - Preto+Branco",
+        Imagem: "https://static.netshoes.com.br/produtos/tenis-adidas-response-runner/26/FB9-3696-026/FB9-3696-026_zoom1.jpg?ts=1704294518&",
+        MiniDescricao: "Estilo e conforto nas corridas e no dia a dia. O tênis adidas Response Runner traz design esportivo leve e macio, com cabedal em malha elástica que proporciona ajuste suave e favorece a circulação de ar.",
+        Descricao: "Estilo e conforto nas corridas e no dia a dia. O tênis adidas Response Runner traz design esportivo leve e macio, com cabedal em malha elástica que proporciona ajuste suave e favorece a circulação de ar. A entressola de EVA eleva o nível de maciez, enquanto o solado de borracha garante tração e durabilidade. Suporte para te acompanhar nas corridas e estilo versátil para usar também nos looks casuais, sempre com bem-estar total. Combine com peças que vão do short esportivo com camiseta lisa ou estampada ao seu jeans favorito. Aproveite!",
+        ValorComDesconto: 218.49,
+        ValorSemDesconto: 299.99
+    },
+    {
+        Titulo: "Tênis Adidas Breaknet Feminino - Branco+Preto",
+        Imagem: "https://static.netshoes.com.br/produtos/tenis-adidas-breaknet-feminino/90/NQQ-4379-890/NQQ-4379-890_zoom1.jpg?ts=1707304065&ims=544x",
+        MiniDescricao: "Aposte em estilo! Trazendo visual urbano super descolado, o Tênis Adidas Breaknet é perfeito para acompanhar as suas produções em qualquer ocasião.",
+        Descricao: "Aposte em estilo! Trazendo visual urbano super descolado, o Tênis Adidas Breaknet é perfeito para acompanhar as suas produções em qualquer ocasião. Confeccionado em material resistente, tem solado emborrachado, palmilha macia, fechamento por cadarço ajustável e mini furos que garantem melhor respirabilidade. Esse tênis feminino exibe as clássicas listras Adidas na lateral e acabamento metálico. Dica: combine com looks casuais ou looks esportivos – calça legging, regata feminina ou sua camiseta estampada favorita. Você merece as melhores escolhas. Garanta já o seu!",
+        ValorComDesconto: 199.99,
+        ValorSemDesconto: 349.99
+    },
+    {
+        Titulo: " Tênis Adidas Ultra Energy Feminino - Verde claro",
+        Imagem: "https://static.netshoes.com.br/produtos/tenis-adidas-ultra-energy-feminino/90/FB8-8826-390/FB8-8826-390_zoom1.jpg?ts=1699634180&ims=544x",
+        MiniDescricao: " Seja você o destaque na corrida com a Adidas! O Tênis Adidas Ultra Energy Feminino é um tênis exclusivo Adidas e Netshoes, a escolha ideal para uma corrida no parque, caminhada e até para o dia a dia para um conforto extra.",
+        Descricao: " Seja você o destaque na corrida com a Adidas! O Tênis Adidas Ultra Energy Feminino é um tênis exclusivo Adidas e Netshoes, a escolha ideal para uma corrida no parque, caminhada e até para o dia a dia para um conforto extra.O cabedal é confeccionado com pelo menos 50 % de conteúdo reciclado, para consciência e sustentabilidade no seu sportwear, oferece respirabilidade e flexibilidade.A entressola Cloudfoam proporciona amortecimento e conforto desde o primeiro passo, já o solado em borracha durável oferece mais resistência ao seu tênis feminino.Use com - short de corrida, regata fitness, top esportivo, tênis Adidas Feminino e esteja sempre em grande estilo com a Adidas!",
+        ValorComDesconto: 199.99,
+        ValorSemDesconto: 379.99
+    },
+    {
+        Titulo: "Tênis Adidas Puremotion 2.0 Farm Feminino - Rosa",
+        Imagem: "https://static.netshoes.com.br/produtos/tenis-adidas-puremotion-20-farm-feminino/34/FB8-7936-034/FB8-7936-034_zoom1.jpg?ts=1706795652&ims=544x",
+        MiniDescricao: "Supere os seus desafios! O Tênis Adidas Puremotion 2.0 Farm é aposta certa para acompanhar os seus treinos ou produções casuais do dia a dia.",
+        Descricao: "Supere os seus desafios! O Tênis Adidas Puremotion 2.0 Farm é aposta certa para acompanhar os seus treinos ou produções casuais do dia a dia. Das rotinas mais agitadas até as mais tranquilas, este tênis Adidas é o que você precisa. Confeccionado em materiais parcialmente reciclados, tem cabedal de malha forrado que garante conforto, enquanto o amortecimento Cloudfoam, combinado com uma palmilha de espuma de memória, envolve os pés com maciez. Elegante e discreto, este tênis feminino é uma colab com a Farm. Garante produção super estilosa. Dica: combine com looks esportivos ou looks casuais - top feminino, regata feminina, camiseta estampada ou calça legging. Você merece as melhores escolhas. Garanta já seu Tênis Adidas Farm!",
+        ValorComDesconto: 284.99,
+        ValorSemDesconto: 449.99
+    },
+    {
+        Titulo: "Tênis Adidas Breaknet Masculino - Branco+Azul",
+        Imagem: "https://static.netshoes.com.br/produtos/tenis-adidas-breaknet-masculino/44/NQQ-4378-044/NQQ-4378-044_zoom1.jpg?ts=1706119674&ims=544x",
+        MiniDescricao: "Estilo atemporal para diversos momentos e combinações. Com modelo clássico, o tênis adidas Breaknet traz as listras icônicas da marca nas laterais, com opções de cores lisas e também animal print.",
+        Descricao: "Estilo atemporal para diversos momentos e combinações. Com modelo clássico, o tênis adidas Breaknet traz as listras icônicas da marca nas laterais, com opções de cores lisas e também animal print. É confeccionado com material resistente e firme, com perfuros para respirabilidade, forro acolchoado e solado resistente ao desgaste. Um tênis masculino versátil para usar com peças que vão da calça jeans à calça jogger ou bermudas masculinas. Conte com a Adidas para complementar o seu look urbano!",
+        ValorComDesconto: 199.99,
+        ValorSemDesconto: 349.99
+    },
+    {
+        Titulo: "Tênis Under Armour Basquete Spawn 3 Masculino - Vermelho+Branco",
+        Imagem: "https://static.netshoes.com.br/produtos/tenis-under-armour-basquete-spawn-3-masculino/56/39W-3127-056/39W-3127-056_zoom1.jpg?ts=1695700529&ims=544x",
+        MiniDescricao: "Dê o seu melhor nas quadras com o Tênis Under Armour Basquete Spawn 3 Masculino. Esse tênis conta com camadas moldadas entrelaçadas para design anatômico que oferece performance.",
+        Descricao: "Dê o seu melhor nas quadras com o Tênis Under Armour Basquete Spawn 3 Masculino. Esse tênis conta com camadas moldadas entrelaçadas para design anatômico que oferece performance. O cabedal conta com estrutura firme e respirável, com suporte no calcanhar e tira de fácil calce para praticidade, tudo pensado na flexibilidade e estabilidade do jogador de basquete. Conta com entressola Cellfit para amortecimento macio e solado Micro G, que transforma cada pouso e impacto em decolagens explosivas. Complemento ideal para usar com bermuda de basquete e camiseta ou regata de basquete da sua equipe favorita. Aposte no design moderno e cheio de tecnologia desse tênis de basquete masculino e converta esse ponto!",
+        ValorComDesconto: 265.99,
+        ValorSemDesconto: 449.99
+    }
+]
+const main = document.querySelector('main')
+produtos.map((pr) => {
+    console.log(pr)
+})
+produtos.map((prod) => {
+    const produto = document.createElement('article')
+    const img = document.createElement('img')
+    const h3 = document.createElement('h3')
+    const h5 = document.createElement('h5')
+    const h2 = document.createElement('h2')
+    const p = document.createElement('p')
+    main.append(produto)
+    img.src = prod.Imagem
+    produto.append(img)
+    h3.innerHTML = prod.Titulo
+    produto.append(h3)
+    h5.innerHTML = prod.ValorSemDesconto
+    produto.append(h5)
+    h2.innerHTML = prod.ValorComDesconto
+    produto.append(h2)
+    p.innerHTML = prod.MiniDescricao
+    produto.append(p)
+})
